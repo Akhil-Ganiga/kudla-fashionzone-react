23d894ad4a1033e57e97e14218d510f1fca43b98
Akhil-Ganiga <akhilakki164@gmail.com>
2024-05-28 22:59:06 +0530

    Deployed Website

--- src/App.js
+++ src/App.js
@@ -1,37 +1,46 @@
-
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import './App.css';
-import Navbar from './Components/Navbar/Navbar';
-import Shop from './Pages/Shop';
-import ShopCategory from './Pages/ShopCategory';
-import Product from './Pages/Product';
-import Cart from './Pages/Cart';
-import LoginSignup from './Pages/LoginSignup';
-import Footer from './Components/Footer/Footer';
-import men_banner from './Components/Assets/banner_mens.png'
-import women_banner from './Components/Assets/banner_women.png'
-import kids_banner from './Components/Assets/banner_kids.png'
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Navbar from "./Components/Navbar/Navbar";
+import Shop from "./Pages/Shop";
+import ShopCategory from "./Pages/ShopCategory";
+import Product from "./Pages/Product";
+import Cart from "./Pages/Cart";
+import LoginSignup from "./Pages/LoginSignup";
+import Footer from "./Components/Footer/Footer";
+import men_banner from "./Components/Assets/banner_mens.png";
+import women_banner from "./Components/Assets/banner_women.png";
+import kids_banner from "./Components/Assets/banner_kids.png";
 
 function App() {
   return (
     <div>
       <BrowserRouter>
-        <Navbar/>
+        <Navbar />
         <Routes>
-          <Route path='/'element={<Shop/>}/>
-          <Route path='/mens'element={<ShopCategory banner={men_banner} category="men"/>}/>
-          <Route path='/womens'element={<ShopCategory banner={women_banner} category="women"/>}/>
-          <Route path='/kids'element={<ShopCategory banner={kids_banner} category="kid"/>}/>
-          <Route path='/product'element={<Product/>}>
-            <Route path=':productId' element={<Product/>}/>
+          <Route path="/kudla-fashionzone-react/" element={<Shop />} />
+          <Route
+            path="/kudla-fashionzone-react/mens"
+            element={<ShopCategory banner={men_banner} category="men" />}
+          />
+          <Route
+            path="/kudla-fashionzone-react/kudla-fashionzone-react/womens"
+            element={<ShopCategory banner={women_banner} category="women" />}
+          />
+          <Route
+            path="/kudla-fashionzone-react/kids"
+            element={<ShopCategory banner={kids_banner} category="kid" />}
+          />
+          <Route path="/kudla-fashionzone-react/product" element={<Product />}>
+            <Route path=":productId" element={<Product />} />
           </Route>
-          <Route path='/cart'element={<Cart/>}/>
-          <Route path='/login'element={<LoginSignup/>}/>
+          <Route path="/kudla-fashionzone-react/cart" element={<Cart />} />
+          <Route
+            path="/kudla-fashionzone-react/login"
+            element={<LoginSignup />}
+          />
         </Routes>
-        <Footer/> 
+        <Footer />
       </BrowserRouter>
-      
     </div>
   );
 }
